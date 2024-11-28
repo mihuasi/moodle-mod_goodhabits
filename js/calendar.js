1cec75909ed71be1db6c14b5a36c3e2e607ec6a1
Joe Cape <joe.sc.cape@gmail.com>
2024-11-28 16:24:02 +0100

    improve responsive design - calendar and simple

--- js/calendar.js
+++ js/calendar.js
@@ -214,6 +214,24 @@ jQuery(window).on('load',function($) {
                 },
             };
 
+
+            const mediaQuery = window.matchMedia("(max-width: 639px)");
+
+            function handleMediaQueryChange(e) {
+                if (e.matches) {
+                    // Media query matches, apply the setting
+                    options.tokenWidth = 30;
+                    console.log("Small screen: tokenWidth set to 30");
+                } else {
+                    // Media query does not match, you can handle other cases here if needed
+                    console.log("Screen is larger than 639px");
+                }
+            }
+
+            mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+            handleMediaQueryChange(mediaQuery);
+
             // options.tokenWidth = 30;
 
             var talentgrid = $('.talentgrid').talentgriddle(options);
