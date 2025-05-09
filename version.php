7154cfbfb1699bc24f969f9d6d543178bd8a4d17
Joe Cape <joe.sc.cape@gmail.com>
2025-05-09 15:39:14 +0200

    version up

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_goodhabits';
 $plugin->release = '2.1.1';
-$plugin->version = 2025050801;
+$plugin->version = 2025050901;
 $plugin->requires = 2019052002;
 $plugin->maturity = MATURITY_RC;
